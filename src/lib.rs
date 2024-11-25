0c32795a74293fe1f7a30bd1dfc668f3428de6ae
Quelfth <nqfrankl@ucsd.edu>
2024-11-25 03:20:26 -0800

    Created the library!

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+
+#[macro_export]
+macro_rules! try_option {
+    ( try {$($t:tt)*} else { $($e:tt)* } ) => {
+        {
+            (|| Some({
+                $($t)*
+            }))().unwrap_or_else (|| { $($e)* })
+        }
+    };
+    ( $($i:tt)* ) => {
+        {
+            (|| Some({
+                $($i)*
+            }))()
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! try_result {
+    ( try {$($t:tt)*} else { $($e:tt)* } ) => {
+        {
+            (|| Ok({
+                $($t)*
+            }))().unwrap_or_else (|_| { $($e)* })
+        }
+    };
+    ( try {$($t:tt)*} else $err:ident { $($e:tt)* } ) => {
+        {
+            (|| Ok({
+                $($t)*
+            }))().unwrap_or_else (|$err| { $($e)* })
+        }
+    };
+    ( try {$($t:tt)*} match { $($e:tt)* } ) => {
+        {
+            (|| Ok({
+                $($t)*
+            }))().unwrap_or_else (|e| match e { $($e)* })
+        }
+    };
+    ( $($i:tt)* ) => {
+        {
+            (|| Ok({
+                $($i)*
+            }))()
+        }
+    };
+}
